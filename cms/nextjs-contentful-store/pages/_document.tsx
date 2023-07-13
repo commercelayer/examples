082f90b2fd95a69db500bfee8de7602bdc2ade71
Bolaji Ayodeji <sirbeejay1@gmail.com>
2023-07-13 10:52:33 +0100

    chore: add dynamic lang attribute to `<Html>`

--- cms/nextjs-contentful-store/pages/_document.tsx
+++ cms/nextjs-contentful-store/pages/_document.tsx
@@ -1,13 +1,23 @@
-import { Head, Html, Main, NextScript } from "next/document";
+import Document, { DocumentContext, Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
-  return (
-    <Html lang="en-US">
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+class MyDocument extends Document {
+  static async getInitialProps(ctx: DocumentContext) {
+    const initialProps = await Document.getInitialProps(ctx);
+    return { ...initialProps };
+  }
+
+  render() {
+    const locale = this.props.__NEXT_DATA__.query.lang as string | undefined;
+    return (
+      <Html lang={locale}>
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
 }
+
+export default MyDocument;
