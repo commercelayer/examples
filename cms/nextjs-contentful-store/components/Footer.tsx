8e670a5b39a7b60d8df67ce74a0ad1d25e3759da
Bolaji Ayodeji <sirbeejay1@gmail.com>
2023-07-13 11:24:44 +0100

    chore: add comment for subscribe form

--- cms/nextjs-contentful-store/components/Footer.tsx
+++ cms/nextjs-contentful-store/components/Footer.tsx
@@ -55,6 +55,7 @@ const Footer: React.FC<Props> = ({ lang, countries, buildLanguages }) => {
               placeholder={locale[lang].subscribePlaceholder}
             />
             <div className="mt-4 sm:ml-4 sm:mt-0 sm:flex-shrink-0">
+              {/* Handle the subcribe form yourself */}
               <button
                 type="submit"
                 className="flex w-full items-center justify-center rounded-md bg-indigo-500 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
