7e76002a93e37911e6a125c2d1684a9dba641946
Matteo Alessani <matteo@commercelayer.io>
2024-08-07 17:28:27 +0200

    fix: update sendgrid example

--- webhooks/sendgrid-templated-emails/mail-server.js
+++ webhooks/sendgrid-templated-emails/mail-server.js
@@ -1,42 +1,42 @@
-require("dotenv").config();
-const express = require("express");
-const CryptoJS = require("crypto-js");
-const hmacSHA256 = require("crypto-js/hmac-sha256");
+require("dotenv").config()
+const express = require("express")
+const CryptoJS = require("crypto-js")
+const hmacSHA256 = require("crypto-js/hmac-sha256")
 
-const app = express();
-const port = 9000;
+const app = express()
+const port = 9000
 
 // Fetch RAW incoming JSON requests and put in req.rawBody
 app.use(
   express.json({
     limit: "5mb",
     verify: (req, res, buf) => {
-      req.rawBody = buf.toString();
-    },
+      req.rawBody = buf.toString()
+    }
   })
-);
+)
 
 app.post("/callback", (req, res) => {
   // Verify the payload coming from Commerce Layer
-  const signature = req.headers["x-commercelayer-signature"];
-  const hash = hmacSHA256(req.rawBody, process.env.CL_SHARED_SECRET);
-  const encode = hash.toString(CryptoJS.enc.Base64);
+  const signature = req.headers["x-commercelayer-signature"]
+  const hash = hmacSHA256(req.rawBody, process.env.CL_SHARED_SECRET)
+  const encode = hash.toString(CryptoJS.enc.Base64)
   if (req.method === "POST" && signature === encode) {
-    const payload = req.body;
+    const payload = req.body
 
     // Filter payload and fetch the required order summary
     const lineItemsArray = payload.included.filter(
       (x) => x.type === "line_items" && x.attributes.item_type === "skus"
-    );
+    )
 
     const filteredLineItemsArray = lineItemsArray.map((item) => {
       return {
         image_url: item.attributes.image_url,
         name: item.attributes.name,
         quantity: item.attributes.quantity,
-        formatted_total_amount: item.attributes.formatted_total_amount,
-      };
-    });
+        formatted_total_amount: item.attributes.formatted_total_amount
+      }
+    })
 
     // You can use the returned values in filteredLineItemsArray to populate your SendGrid template
     // But you will have to adjust the template to use the {{#each }} helper function
@@ -66,11 +66,12 @@ app.post("/callback", (req, res) => {
       marketName: payload.included[0].attributes.name,
       paymentMethod: payload.included[4].attributes.name,
       shippingAddress: payload.included[2].attributes.full_address,
-      shipmentNumber: payload.included.find((x) => x.type === "shipments")
-        .attributes.number,
-      shippingMethod: payload.included.find(
-        (x) => x.type === "shipping_methods"
-      ).attributes.name,
+      shipmentNumber:
+        payload.included.find((x) => x.type === "shipments")?.attributes
+          ?.number ?? "no shipment required",
+      shippingMethod:
+        payload.included.find((x) => x.type === "shipping_methods")?.attributes
+          ?.name ?? "no shipment required",
       lineItemsHtml:
         `<table class="line-items">` +
         filteredLineItemsArray
@@ -82,23 +83,23 @@ app.post("/callback", (req, res) => {
         `</table>`,
       totalAmount: payload.data.attributes.formatted_subtotal_amount,
       shippingAmount: payload.data.attributes.formatted_shipping_amount,
-      grandTotalAmount: payload.data.attributes.formatted_total_amount,
-    };
+      grandTotalAmount: payload.data.attributes.formatted_total_amount
+    }
 
     // Send Email with SendGrid
-    const sgMail = require("@sendgrid/mail");
-    sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+    const sgMail = require("@sendgrid/mail")
+    sgMail.setApiKey(process.env.SENDGRID_API_KEY)
 
     sgMail
       .send({
         to: inputData.customerEmail,
         from: {
           email: "bolaji@commercelayer.io",
-          name: "Cake Store Team",
+          name: "Cake Store Team"
         },
         reply_to: {
           email: "bolaji@commercelayer.io",
-          name: "No Reply",
+          name: "No Reply"
         },
         templateId: process.env.SENDGRID_TEMPLATE_ID,
         dynamicTemplateData: {
@@ -115,29 +116,32 @@ app.post("/callback", (req, res) => {
           lineItems: inputData.lineItemsHtml,
           totalAmount: inputData.totalAmount,
           shippingAmount: inputData.shippingAmount,
-          grandTotalAmount: inputData.grandTotalAmount,
-        },
+          grandTotalAmount: inputData.grandTotalAmount
+        }
       })
       .then((response) => {
-        console.log(response);
+        console.log("response")
+
+        console.log(response)
         res.status(200).json({
           message: `Email sent to customer (${inputData.customerEmail})!`,
-          response: response,
-        });
+          response: response
+        })
       })
       .catch((error) => {
-        console.error(error);
+        console.log("error")
+
         res.status(500).json({
-          error: error,
-        });
-      });
+          error: error
+        })
+      })
   } else {
     res.status(401).json({
-      error: "Unauthorized: Invalid signature",
-    });
+      error: "Unauthorized: Invalid signature"
+    })
   }
-});
+})
 
 app.listen(port, () => {
-  console.log(`Listening at http://localhost:${port}`);
-});
+  console.log(`Listening at http://localhost:${port}`)
+})
