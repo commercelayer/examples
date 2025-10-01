d99f74d0b0ba4b651db8a104c22be9ec56a573b7
Matteo Alessani <matteo@commercelayer.io>
2025-10-01 21:23:40 +0200

    chore: remove hero from homepage

--- authentication/nextjs-auth0-sso/app/page.jsx
+++ authentication/nextjs-auth0-sso/app/page.jsx
@@ -2,15 +2,10 @@
 
 import React from 'react'
 
-import Hero from '../components/Hero'
 import Content from '../components/Content'
 
 export default function Index() {
   return (
-    <>
-      <Hero />
-      <hr />
-      <Content />
-    </>
+    <Content />
   )
 }
